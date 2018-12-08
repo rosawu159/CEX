739a1a34830871b32e77228c9572e9fd5f55594a
Rosa Wu <u10050070@gmail.com>
2018-12-08 19:55:14 +0800

    Update popup.js

--- chromeEX1/popup.js
+++ chromeEX1/popup.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-	chrome.tabs.query(
-        {'active': true, 'lastFocusedWindow': true},
-        function (tabs) {
-            var url = tabs[0].url;
-            $("#msgLabel").text("waiting...."); 
-            $("#msgLabel").text(url);   
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url0 = tabs[0].url;
+    $("#msgLabel").text(url0);
+    $.ajax({
+        url: "http://130.211.255.166/d",
+        type:"GET",
+        data:{q: url0,
+            format:"json"},
+        dataType: "JSONP",
+        jsonp: false, 
+        success: function(data){
+            console.log(data)
         }
-    );
+    })   
+    });
 });
