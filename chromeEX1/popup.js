b806f116567e3ef69fa9adfb1a77c6464f3ac44b
Rosa Wu <u10050070@gmail.com>
2019-01-05 18:34:41 +0800

    calculate score

--- chromeEX1/popup.js
+++ chromeEX1/popup.js
@@ -7,10 +7,32 @@ document.addEventListener('DOMContentLoaded', function () {
         url: "http://localhost:5000/dataconvector",
         crossDomain:true,
         data: JSON.stringify({"url":url0}),
-        dataType:"JSON"
+        dataType:"JSON",
+        beforeSend: function(){
+            $("#text").text("正在計算網站的不信任指數...");
+            $("#loadingIMG").show();
+            
+        }
     }).done(function(responseData){
         var a=responseData.result
-        $("#result").text(a);
+        var score=a*20+5
+        $("#text").text("此網站的不信任指數為");
+        $("#result").text(score+"%");
+        $("#loadingIMG").hide();
+        var img = document.createElement("img");
+        if (a<=1){
+            img.src = "0.png";}
+        else if(a>1 && a<=2){
+            img.src = "1.png";}
+        else if(a>2 && a<=3){
+            img.src = "2.png";}
+        else if(a>3 && a<=4){
+            img.src = "3.png";}
+        else if(a>4 && a<=5){
+            img.src = "4.png";}
+        
+        var src = document.getElementById("emoji");
+        src.appendChild(img);
     });
     
 
