a3fb185a75d5e024fec31a01f51952aadb00464f
Rosa Wu <u10050070@gmail.com>
2019-03-06 16:56:56 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -1,12 +1,9 @@
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 from testsearchforwin import searchall
-from linebot import (
-    LineBotApi, WebhookHandler)
-from linebot.exceptions import (
-    InvalidSignatureError)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage, ImageSendMessage)
 
 app = Flask(__name__)
 CORS(app, resources=r'/*')
@@ -15,7 +12,6 @@ CORS(app, resources=r'/*')
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
@@ -35,14 +31,12 @@ def handle_message(event):
             event.reply_token,[
             #TextSendMessage(text=event.message.text),
             TextSendMessage(text="危險指數：" + a[3] + "%"),
-            TextSendMessage(text="公司資訊："+a[2]+"\n最後更新："+a[0]+"\n到期日期："+a[1])]
-        )
+            TextSendMessage(text="公司資訊："+a[2]+"\n最後更新："+a[0]+"\n到期日期："+a[1])])
     else:
         line_bot_api.reply_message(
             event.reply_token,[
             TextSendMessage(text=event.message.text),
-            TextSendMessage(text="無法得知公司資訊")]
-        )
+            TextSendMessage(text="無法得知公司資訊")])
 
 if __name__ == "__main__":
     app.run()
